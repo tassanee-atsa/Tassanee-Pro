7398aeaa69683b23cc50f482163f04248a7015a3
Tassanee Atsawajuramanee <tassanee.atsa@gmail.com>
2022-11-30 20:48:41 +0000

    edit contact info

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -19,13 +19,13 @@ const Contact = () => {
           {/* left */}
           <div className='col-span-3 lg:col-span-2 w-full h-full shadow-xl shadow-gray-400 rounded-xl p-4'>
             <div className='lg:p-4 h-full '>
-              <div>
+              {/* <div>
                 <Image
                   className='rounded-xl hover:scale-105 ease-in duration-300'
                   src={ContactImg}
                   alt='/'
                 />
-              </div>
+              </div> */}
               <div>
                 <h2 className='py-2'>Tassanee Atsawajuramanee</h2>
                 <p>Full Stack Developer</p>
